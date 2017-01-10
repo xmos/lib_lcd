36da05ec7d8ed74a0091e5365defac8b9fa9220e
Sam Chesney <samc@xmos.com>
2017-01-10 10:19:31 +0000

    Remove build directory

--- tests/lcd_testbench_port32/.build/home/sudha/XMOS/library/LCD/lib_lcd/lib_lcd/src/lcd.xc.d
+++
@@ -1,3 +0,0 @@
-.build/home/sudha/XMOS/library/LCD/lib_lcd/lib_lcd/src/lcd.xc.d ./.build/home/sudha/XMOS/library/LCD/lib_lcd/lib_lcd/src/lcd.xc.o: ../../lib_lcd/src/lcd.xc \
-  ../../lib_lcd/api/lcd.h
-
--- tests/lcd_testbench_port32/.build/src/lcd_testbench.xc.d
+++
@@ -1,3 +0,0 @@
-.build/src/lcd_testbench.xc.d ./.build/src/lcd_testbench.xc.o: ./src/lcd_testbench.xc \
-  ../../lib_lcd/api/lcd.h
-
